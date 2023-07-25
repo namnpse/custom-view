2bface2a62ffe18f8437483fdf42601a470dbbb5
Namnpse <nam444479@gmail.com>
2023-07-25 09:45:41 +0700

    draw objects of the face using paint, customize width, height

---
+++ app/src/main/java/com/namnp/customview/emotional_face/EmotionalFaceActivity.kt
@@ -0,0 +1,12 @@
+package com.namnp.customview.emotional_face
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.namnp.customview.R
+
+class EmotionalFaceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_emotional_face)
+    }
+}
